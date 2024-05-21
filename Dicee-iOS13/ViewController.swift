658ed445baff0dcae8da26bf763e2c44029c05b2
Amr-sami <samyamr819@gmail.com>
2024-05-21 23:48:57 +0300

    Score Tracking

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,34 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var score = 0
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let diceArray = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ]
+        
+        let firstDiceIndex = Int.random(in: 0...5)
+        let secondDiceIndex = Int.random(in: 0...5)
+        
+        diceImageView1.image = diceArray[firstDiceIndex]
+        diceImageView2.image = diceArray[secondDiceIndex]
+        
+        if diceImageView1.image == diceImageView2.image {
+            score += 1
+            scoreLabel.text = "Score: \(score)"
+        }
+        print("Score: \(score)")
     }
-
-
 }
-
